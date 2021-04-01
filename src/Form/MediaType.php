b467f616105a38e0569af6ff05e2f7f87da0477f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-01 18:11:25 +0200

    Validate MIME types

--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class MediaType extends AbstractType
@@ -14,7 +15,13 @@ class MediaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('file', VichFileType::class)
+            ->add('file', VichFileType::class, [
+                'constraints' => new File(mimeTypes: [
+                    'video/x-matroska',
+                    'video/mp4',
+                    'video/x-msvideo'
+                ]),
+            ])
             ->add('submit', SubmitType::class);
     }
 
