8cc3e7c8221f7a533f0cfd62df38452d7601d7c6
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-01 17:08:26 +0200

    List names of all uploads in the dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,11 @@ class DashboardController extends AbstractController
     #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig');
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->render('dashboard/index.html.twig', [
+            'userContent' => $user->getMedia()
+        ]);
     }
 }
