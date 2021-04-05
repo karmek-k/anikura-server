713cdce686bf141462b90b09552529231557a755
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-05 17:26:04 +0200

    Fix navbar collapse not working

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 import '@fortawesome/fontawesome-free/js/all';
 // import '@fortawesome/fontawesome-free/css/all.css';
