cbdef60f3648507f2091e7135e326abe06198741
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-06 18:41:24 +0200

    Create settings_create_user action

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Service\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +17,39 @@ class SettingsController extends AbstractController
     {
         return $this->render('settings/index.html.twig', [
             'controller_name' => 'SettingsController',
+        ]);
+    }
+
+    #[Route('/settings/create-user', name: 'settings_create_user')]
+    public function createUser(Request $request, UserFactory $userFactory): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $username = $user->getUsername();
+            $password = $user->getPassword();
+
+            $newUser = $userFactory->createUser(
+                $username,
+                $password,
+                admin: false
+            );
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($newUser);
+            $manager->flush();
+
+            $this->addFlash('User successfully created!', 'success');
+
+            return $this->redirectToRoute('settings');
+        }
+
+        return $this->render('settings/create_user.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
