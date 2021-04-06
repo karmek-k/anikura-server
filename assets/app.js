c0a3efc34861a5772a4c84ceef7552fccc1b7af4
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-06 20:12:41 +0200

    Reduce bundle size by limiting icon count

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
-import '@fortawesome/fontawesome-free/js/all';
-// import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
 
 import './css/forms.css';
